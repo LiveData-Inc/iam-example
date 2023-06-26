4fff5a60cc30461923c48053b7d5eddf0c54c74e
Jeff Robbins <79278744+jrobbins-LiveData@users.noreply.github.com>
2023-06-26 08:09:07 -0400

    managed role attempt

--- iam_example/iam_example_stack.py
+++ iam_example/iam_example_stack.py
@@ -9,31 +9,38 @@ class IamExampleStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        policies = [
-            iam.PolicyStatement(
-                sid='IamExampleEventsActions',
-                effect=iam.Effect.ALLOW,
-                actions=[
-                    'events:DisableRule',
-                    'events:EnableRule',
-                ],
-                resources=[
-                    self.format_arn(
-                        service='events',
-                        resource='rule',
-                        resource_name='test_rule',
-                    ),
-                ]
-            ),
-        ]
-
+        managed_policy = iam.ManagedPolicy(
+            self, 'LambdaManagedPolicy',
+            description='Allow Lambda access to Log and Event',
+            statements=[
+                iam.PolicyStatement(
+                    sid='IamExampleEventsActions',
+                    effect=iam.Effect.ALLOW,
+                    actions=['events:DisableRule', 'events:EnableRule'],
+                    resources=[self.format_arn(service='events', resource='rule', resource_name='test_rule')]
+                ),
+            ]
+        )
+        lambda_role = iam.Role(
+            self, 'LambdaRole',
+            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
+            managed_policies=[managed_policy]
+        )
         handler = lambda_.Function(
             self, "TestFunction",
             runtime=lambda_.Runtime.PYTHON_3_10,
             handler="hello.handler",
             code=lambda_.Code.from_asset("lambda_src"),
             reserved_concurrent_executions=10,  # HIPAA.Security-LambdaConcurrency
-            initial_policy=policies
+            role=lambda_role
+        )
+        managed_policy.add_statements(
+            iam.PolicyStatement(
+                sid='IamExampleLogAccess',
+                effect=iam.Effect.ALLOW,
+                actions=['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+                resources=[handler.log_group.log_group_arn],
+            )
         )
 
         NagSuppressions.add_resource_suppressions(
