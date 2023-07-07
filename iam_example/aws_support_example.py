35ba5a54e78408d917006eabb315da25b32c5dda
Jeff Robbins <79278744+jrobbins-LiveData@users.noreply.github.com>
2023-07-07 12:02:31 -0400

    explicitly create Log Group

--- iam_example/aws_support_example.py
+++ iam_example/aws_support_example.py
@@ -1,33 +1,28 @@
-from aws_cdk import Stack
+from aws_cdk import Stack, Aws
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_kms as kms
-from aws_cdk import aws_lambda as lambda_
 from aws_cdk import aws_logs as logs
-from aws_cdk.aws_logs import RetentionDays
+from aws_cdk import aws_lambda as lambda_
 from cdk_nag import NagSuppressions
 from constructs import Construct
 
-FUNCTION_NAME = 'TestFunction'
-NOT_AN_ISSUE = 'NOT AN ISSUE: suppressing for this demo'
 
+class CustomNagStack(Stack):
 
-class IamExampleStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Without this Key, we'd break this Rule:
-        # HIPAA.Security-CloudWatchLogGroupEncrypted: The CloudWatch Log Group is not encrypted with an AWS KMS key
-        key = kms.Key(self, 'TestKey', enable_key_rotation=True)
+        FUNCTION_NAME = 'My-Test-Function'
+
+        #Key is necessary other wise Rule breaks > HIPAA.Security-CloudWatchLogGroupEncrypted: The CloudWatch Log Group is not encrypted with an AWS KMS key
+        key = kms.Key(self, "Key",enable_key_rotation=True)
         key.grant_encrypt_decrypt(iam.ServicePrincipal(f'logs.{self.region}.amazonaws.com'))
 
-        # Manually create our Lambda Function's CloudWatch Log Group
-        log_group = logs.LogGroup(
-            self, 'TestLogGroup',
+        #Creating a Lambda Log Group beforehand.
+        log_group = logs.LogGroup(self, "LogGroup",
             log_group_name=f'/aws/lambda/{FUNCTION_NAME}',
-            encryption_key=key,
-            retention=RetentionDays.SEVEN_YEARS
+            encryption_key=key
         )
-
         lambda_policy = iam.ManagedPolicy(
             self, 'LambdaManagedPolicy',
             description='Allow Lambda access to Log and Event',
@@ -38,12 +33,12 @@ class IamExampleStack(Stack):
                     actions=['events:DisableRule', 'events:EnableRule'],
                     resources=[self.format_arn(service='events', resource='rule', resource_name='test_rule')]
                 ),
-                iam.PolicyStatement(
-                    sid='IamExampleLambdaLogAccess',
-                    effect=iam.Effect.ALLOW,
-                    actions=['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
-                    resources=[log_group.log_group_arn],
-                )
+                 iam.PolicyStatement(
+                sid='IamExampleLambdaLogAccess',
+                effect=iam.Effect.ALLOW,
+                actions=['logs:CreateLogStream', 'logs:PutLogEvents'],
+                resources=[log_group.log_group_arn]
+                 )
             ]
         )
         lambda_role = iam.Role(
@@ -51,11 +46,9 @@ class IamExampleStack(Stack):
             assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
             managed_policies=[lambda_policy]
         )
-
         handler = lambda_.Function(
             self, 'TestFunction',
-            # explicit function_name to match the above-created `log_group`
-            function_name=FUNCTION_NAME,
+            function_name=FUNCTION_NAME, #Explicitly function_name providing is necessary since we are separately creating a log group using the same name. 
             runtime=lambda_.Runtime.PYTHON_3_10,
             handler='hello.handler',
             code=lambda_.Code.from_asset('lambda_src'),
@@ -63,33 +56,16 @@ class IamExampleStack(Stack):
             role=lambda_role
         )
 
-        lambda_policy.add_statements(
-            iam.PolicyStatement(
-                sid='IamExampleLambdaLogAccess',
-                effect=iam.Effect.ALLOW,
-                actions=['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
-                resources=[handler.log_group.log_group_arn],
-            )
-        )
-
         NagSuppressions.add_resource_suppressions(
             handler, [
                 dict(
                     id='HIPAA.Security-LambdaDLQ',
-                    reason=NOT_AN_ISSUE
+                    reason='NOT AN ISSUE: suppressing for this demo'
                 ),
                 dict(
                     id='HIPAA.Security-LambdaInsideVPC',
-                    reason=NOT_AN_ISSUE
+                    reason='NOT AN ISSUE: suppressing for this demo'
                 ),
             ]
         )
 
-        NagSuppressions.add_resource_suppressions(
-            lambda_policy, [
-                dict(
-                    id='AwsSolutions-IAM5',
-                    reason='Log Group requires wildcard permission for associated Log Streams'
-                )
-            ]
-        )
